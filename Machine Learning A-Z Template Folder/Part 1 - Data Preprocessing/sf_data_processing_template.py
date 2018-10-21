e2a81a409d19c3bf7fd5c08464682f836e4279c0
Sirfame Lin <sirfame@hotmail.com>
2018-10-20 22:02:55 -0700

    calculated mean of columns to fill in for missing vales

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/sf_data_processing_template.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/sf_data_processing_template.py
@@ -20,3 +20,22 @@ On the right of the comma, we have a colon -1, meaning we take all the columns e
 .values takes all the values
 
 """
+
+y = dataset.iloc[:, :3].values
+
+
+# taking care of missing data.
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+"""
+the missing_values parameter will look for values that are Null, which is represented by NaN, even though the
+data says nan in lowercase
+
+Next we have to set the strategy, and in our case, we are calculating the mean.
+
+The third parameter is either 1 or 0, 1 being the mean if the rows, and 0 being the mean of the columns.
+"""
+
+imputer = imputer.fit(X[:, 1:3]) #upper bound is excluded.
+
+X[:, 1:3] = imputer.transform(X[:, 1:3])
