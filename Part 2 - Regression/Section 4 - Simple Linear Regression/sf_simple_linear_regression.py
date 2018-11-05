8a098c77a44197fd0fa01dcca1225e525b1851a8
Sirfame Lin <sirfame@hotmail.com>
2018-11-05 08:00:20 -0800

    finished simple linear regression in python

--- Part 2 - Regression/Section 4 - Simple Linear Regression/sf_simple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/sf_simple_linear_regression.py
@@ -22,3 +22,28 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Simple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Visualizing the Training set Results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualizing the Test set Results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
