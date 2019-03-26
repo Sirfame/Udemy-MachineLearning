5197f3ddcb9ff65cc73eab3350afd53e4c78451f
Sirfame Lin <sirfame@hotmail.com>
2019-03-26 07:56:59 -0700

    fitting, predicting, and confusion matrix

--- Part 3 - Classification/Section 14 - Logistic Regression/sf_logistical_regression.py
+++ Part 3 - Classification/Section 14 - Logistic Regression/sf_logistical_regression.py
@@ -19,5 +19,15 @@ from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)
+
+# Fitting logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
